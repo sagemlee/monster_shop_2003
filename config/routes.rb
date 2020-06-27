321c3b3597a6ae851b8fc39166878f4283c70ebe
Sage Lee <sagemcklee@gmail.com>
2020-06-27 15:06:34 -0600

    Update merchant, items, reviews and users to resources

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,10 @@ Rails.application.routes.draw do
     get '/merchants/:merchant_id', to: "merchants#show"
     get '/merchants', to: "merchants#index"
     get "/merchants/status/:id", to: "merchants#update"
+    
     get '/users', to: 'users#index'
     get '/profile/:user_id', to: 'users#show'
+
     patch '/orders/:order_id', to: 'orders#update'
     get "/users/:user_id/orders/:order_id", to: "orders#show"
   end
@@ -24,13 +26,14 @@ Rails.application.routes.draw do
   namespace :merchant do
     get '/orders/:order_id', to: 'orders#show'
     patch '/orders/:order_id', to: 'orders#update'
+
     get '/items', to: 'items#index'
     get '/items/:id/status', to: 'items#status'
     get '/items/:id/edit', to: 'items#edit'
     patch '/items/:id', to: 'items#update'
-    delete '/items/:id', to: 'items#destroy'
     get '/items/new', to: 'items#new'
     post '/items', to: 'items#create'
+    delete '/items/:id', to: 'items#destroy'
   end
 
   resources :merchants
@@ -57,41 +60,34 @@ Rails.application.routes.draw do
 
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
-
+ 
   get "/merchants/:merchant_id/items", to: "merchants_items#index"
   get "/merchants/:merchant_id/items/new", to: "merchants_items#new"
   post "/merchants/:merchant_id/items", to: "merchants_items#create"
-
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  
+  resources :reviews, only: [:edit, :update, :destroy]
+  # get "/reviews/:id/edit", to: "reviews#edit"
+  # patch "/reviews/:id", to: "reviews#update"
+  # delete "/reviews/:id", to: "reviews#destroy"
 
   get "/cart", to: "cart#show"
   delete "/cart", to: "cart#destroy"
 
   resources :users, only: [:create]
   #post "/users", to: "users#create"
-  # resources :profile do 
-  #   resources :users, only: [:edit, :show, :update]
-  # end 
+
   get "/profile", to: "users#show"
   get "/profile/edit", to: "users#edit"
   post "/profile", to: "users#update"
-
   get "/register", to: "users#new"
 
+  #resources :password, only: [:edit, :update]
   get "/password/edit", to: "password#edit"
   post "/password", to: "password#update"
 
-  resources :orders
+  resources :orders, except: [:destroy]
   # get "/orders/new", to: "orders#new"
   # post "/orders", to: "orders#create"
-  
-  # shallow do 
-  #   resources :profile do
-  #   resources :orders
-  #   end 
-  # end  
   # get "/profile/orders/:id", to: "orders#show"
   # get "/profile/orders", to: "orders#index"
   # patch "/profile/orders/:id", to: "orders#update"
